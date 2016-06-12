fixed A_c calculation, although the antithetic techniques gives the same mean value across all simulations (unsure  if correct)

M4/Exam/HJM_caplet_pricing_v2.py
@@ -38,7 +38,7 @@ import sys
 # Define parameters values
 T = 10.0  # t_max or maturity
 M = 1000  # no. of time steps
-I = 2  # no. of MC simulations (S paths)
+I = 3000  # no. of MC simulations (S paths)
 n_tau = 50  # no. of tenors
 
 # Derived params
@@ -56,7 +56,8 @@ S0 = np.array(data.iloc[4, :])  # initial forward rate f(t=0, T)
 
 # Calculate the dtau and dF for Musiela correction term dF/dtau in forward rate
 # Must append last value again to get correct vector dimension - this means 25Y maturity uses a backward derivative
-d_tau = np.diff(np.array(data.columns, dtype=np.float))  # convert from string to float to take diff
+tau = np.array(data.columns, dtype=np.float)
+d_tau = np.diff(tau)  # convert from string to float to take diff
 d_tau = np.append(d_tau, d_tau[-1])  # this is dtau
 d_S_plus = np.diff(S0)
 d_S_plus = np.append(d_S_plus, d_S_plus[-1])  # this is dF
@@ -75,7 +76,7 @@ d_tau_m = np.zeros(shape_3D, dtype=np.float)
 t_index = np.zeros((M + 1), dtype=np.float)  # note this is a 2D array
 d_S_plus_m = np.zeros(shape_3D, dtype=np.float)
 S_plus_m = np.zeros(shape_3D, dtype=np.float)
-# A_c_plus_m = np.zeros(shape_3D, dtype=np.float)  # arithmetic continuous average
+
 
 # Set first value of arrays (row=0, t=0) since loop starts at row=1
 mu_m[0][:] = mu
@@ -86,25 +87,23 @@ d_tau_m[0][:] = d_tau
 d_S_plus_m[0][:] = d_S_plus
 t_index[0] = 0
 S_plus_m[0][:] = S0
-# A_c_plus_m[0][:] = S0  # same as S_plus_m
 
 # Create 'minus' array for S and dS to apply antithetic variance reduction technique
 # warning: must ensure plus and minus objects are set via copy() instead of '=' otherwise memory issues
 S_minus_m = S_plus_m.copy()
-# A_c_minus_m = A_c_plus_m.copy()
 d_S_minus_m = d_S_plus_m.copy()
 
 # Define random number generator seed - this makes tne RNs predictable, otherwise get different everytime
 np.random.seed(1000)
 
-# *** Below is only for testing purposes (to replicate excel results) ***
-RNs = pd.read_csv("temp_RNs.csv")
-rand1_m = np.array(RNs.iloc[:, 0])
-rand1_m = np.insert(rand1_m, 0, rand1_m[0])
-rand2_m = np.array(RNs.iloc[:, 1])
-rand2_m = np.insert(rand2_m, 0, rand2_m[0])
-rand3_m = np.array(RNs.iloc[:, 2])
-rand3_m = np.insert(rand3_m, 0, rand3_m[0])
+# # *** Below is only for testing purposes (to replicate excel results) ***
+# RNs = pd.read_csv("temp_RNs.csv")
+# rand1_m = np.array(RNs.iloc[:, 0])
+# rand1_m = np.insert(rand1_m, 0, rand1_m[0])
+# rand2_m = np.array(RNs.iloc[:, 1])
+# rand2_m = np.insert(rand2_m, 0, rand2_m[0])
+# rand3_m = np.array(RNs.iloc[:, 2])
+# rand3_m = np.insert(rand3_m, 0, rand3_m[0])
 
 # Loop over time - start from 2nd row since we have set initial values
 for i in xrange(1, M + 1, 1):
@@ -118,18 +117,18 @@ for i in xrange(1, M + 1, 1):
     vol3_m[i][:] = vol3
     d_tau_m[i][:] = d_tau
 
-    # # Get I RNs sampled from standard normal distribution
-    # rand1 = np.random.standard_normal((I, 1))
-    # rand2 = np.random.standard_normal((I, 1))
-    # rand3 = np.random.standard_normal((I, 1))
+    # Get I RNs sampled from standard normal distribution
+    rand1 = np.random.standard_normal((I, 1))
+    rand2 = np.random.standard_normal((I, 1))
+    rand3 = np.random.standard_normal((I, 1))
 
-    # *** Below is only for testing purposes (to replicate excel results) ***
-    rand1 = np.array([[rand1_m[i]], [np.random.standard_normal()]])
-    rand2 = np.array([[rand2_m[i]], [np.random.standard_normal()]])
-    rand3 = np.array([[rand3_m[i]], [np.random.standard_normal()]])
-    # rand1 = rand_m[i]
-    # rand2 = rand2_m[i]
-    # rand3 = rand3_m[i]
+    # # *** Below is only for testing purposes (to replicate excel results) ***
+    # rand1 = np.array([[rand1_m[i]], [np.random.standard_normal()]])
+    # rand2 = np.array([[rand2_m[i]], [np.random.standard_normal()]])
+    # rand3 = np.array([[rand3_m[i]], [np.random.standard_normal()]])
+    # # rand1 = rand_m[i]
+    # # rand2 = rand2_m[i]
+    # # rand3 = rand3_m[i]
 
     # ----- DYNAMIC -----
 
@@ -152,10 +151,7 @@ for i in xrange(1, M + 1, 1):
                       mu_m[i][:] * dt + \
                       (vol1_m[i][:] * (-rand1) + vol2_m[i][:] * (-rand2) + vol3_m[i][:] * (-rand3)) * math.sqrt(dt) + \
                       (d_S_minus_m[i][:] / d_tau_m[i][:]) * dt
-    
-    # # Arithmetic continuous average must be set equal to S to calculate the true values later
-    # A_c_plus_m[i][:] = S_plus_m[i][:]
-    # A_c_minus_m[i][:] = S_minus_m[i][:]
+
 
 ############################################################################################
 
@@ -194,18 +190,16 @@ for i in xrange(1, M + 1, 1):
 
 # Join plus and minus stats (antithetic technique)
 S_join_m = np.concatenate((S_plus_m, S_minus_m), axis=1)  # shape (M + 1, 2 * I, n_tau)
-# A_c_join_m = np.concatenate((S_plus_m, S_minus_m), axis=1)  # shape (M + 1, 2 * I, n_tau)
-# A_c_join_m = np.concatenate((A_c_plus_m, A_c_minus_m), axis=1)  # shape (M + 1, 2 * I, n_tau)
 
 # --------------------------------
-#       EXPANDING MEAN
+#       ROLLING MEAN
 # --------------------------------
 
-# Arithmetic continuous expanding mean
+# Arithmetic continuous running average
 A_c_plus_m = S_plus_m.copy()
 A_c_minus_m = S_minus_m.copy()
-A_c_join_m = S_plus_m.copy()  # antithetic version, for now set to same no. of dims as S_plus to plot it against it
-# A_c_join_m[:][0] = 0.5 * (A_c_plus_m[:][0] + A_c_minus_m[:][0])
+A_c_join_m = np.zeros(shape_3D, dtype=np.float)  # antithetic version, for now set to same no. of dims as S_plus to plot it against it
+A_c_join_m[:, 0, :] = 0.5*(A_c_plus_m[:, 0, :] + A_c_minus_m[:, 0, :])
 
 for i in xrange(1, I):
     # loop over all simulations for all tenors and times
@@ -217,7 +211,9 @@ for i in xrange(1, I):
 #       PLOTTING
 # -------------------------
 
-sys.exit()
+# # Convergence diagram
+# sns.pointplot(np.arange(I), A_c_plus_m[1, :, 0])  # f(t=0.01, tau=0) vs number of simulations
+# sns.pointplot(np.arange(I), A_c_join_m[1, :, 0])  # same as above but using antithetic technique
 
 # p = sns.pointplot(np.arange(I), A_c_plus_m[1, :, 0])
 # # p.xaxis.set_visible(False)
@@ -225,98 +221,83 @@ sys.exit()
 # x_values = [0, 20, 40, 60, 80, 100]
 # p.xaxis.set_ticklabels(x_values)
 
-sns.pointplot(np.arange(I), A_c_plus_m[1, :, 0])
-sns.pointplot(np.arange(I), A_c_join_m[1, :, 0])
-
 
 ############################################################################################
 
-
-# ----------- PRICING
-a = np.mean(S_plus_m, axis=1)
-b = np.mean(S_join_m, axis=1)
-
-# Rolling mean
+# -------------------------
+#       PRICING
+# -------------------------
 
 
-# y = np.zeros(shape_3D, dtype=np.float)
+# # Average across all simulations
+# # a_plus = np.mean(S_plus_m, axis=1)
+# a_plus = A_c_plus_m[:, -1, :]  # above line is equivalent to getting the last element of A_c_plus
+#
+# # a_minus = np.mean(S_minus_m, axis=1)
+# a_minus = A_c_minus_m[:, -1, :]  # above line is equivalent to getting the last element of A_c_minus
+#
+# # a_join = np.mean(S_join_m, axis=1)
+# a_join = A_c_join_m[:, -1, :]  # above line is equivalent to getting the last element of A_c_join
+
+# -------------------------
+#       Caplet
+# -------------------------
+
+#  Price a caplet option written on 6M LIBOR starting six months from today
+
+# Convert forward rate to LIBOR
+my_T_0 = 0.5  # LIBOR fixed
+my_T_1 = 1.0  # Cashflow paid
+my_tau = my_T_1 - my_T_0  # tenor
+my_t = 0.50  # future time (want expectation of 6M LIBOR)
+
+my_DF = 0.996  # discount factor
+my_notional = 100000.0  # notional
+my_K = 0.035  # strike
+
+# # *** Below values are just for testing benchmark ***
+# my_DF = 0.9518545  # discount factor
+# my_notional = 1.0  # notional
+# my_K = 0.03  # strike
+
+my_t_loc = np.where(np.round(t_index, 2) == np.round(my_t, 2))[0][0]  # locate the t index for my_t, must round
+my_tau_loc = np.where(np.round(tau, 2) == np.round(my_tau, 2))[0][0]  # locate the tau index for my_tau, must round
+
+my_f_plus = A_c_plus_m[my_t_loc, :, my_tau_loc]
+my_f_join = A_c_join_m[my_t_loc, :, my_tau_loc]
+
+my_L_plus = (1.0 / my_tau) * (np.exp(my_f_plus * my_tau) - 1)
+my_L_join = (1.0 / my_tau) * (np.exp(my_f_join * my_tau) - 1)
+
+my_cap_plus = my_DF * np.maximum(my_L_plus - my_K, 0) * my_tau * my_notional
+my_cap_join = my_DF * np.maximum(my_L_join - my_K, 0) * my_tau * my_notional
 
 
+# -------------------------
+#       PLOTTING
+# -------------------------
 
-## ---------- Plotting
+# Convergence diagram
+plt.plot(np.arange(I), my_cap_plus, '.-', ms=10, label='without Antith.')
+# plt.plot(np.arange(I), my_cap_plus, '.', ms=10, label='without Antith.', color='blue')
+# plt.plot(np.arange(I), my_cap_plus, '-', ms=1, label='without Antith.', color='blue')
+plt.plot(np.arange(I), my_cap_join, '.-', ms=7, label='with Antith.')
+plt.xlabel('No. of MC simulations')
+plt.ylabel('Caplet price')
+plt.legend()
 
-
-
-# max_xticks = 10
-# xloc = plt.MaxNLocator(max_xticks)
-# p.xaxis.set_major_locator(xloc)
-
-plt.show()
-# df_plus = pd.DataFrame(data=A_c_plus_m[1, :, 0], columns=['V'])
-
-# Calculate option value for Asian and EU (to have EU as benchmark)
-
-# ----------------------------------------------------
-# EUROPEAN CALL - using antithetic variance reduction
-# ----------------------------------------------------
-DF = math.exp(-r * T)
-V_join = DF * np.maximum(S_join - K, 0)
-V = np.mean(V_join[-1])
-V_e = np.std(V_join[-1]) / I  # error std/sqrt(N)
-
-V_plus = DF * np.maximum(S_plus[-1] - K, 0) # just for demonstration purposes
-
-# --------------------------------------------------
-# ASIAN CALL - using antithetic variance reduction
-# --------------------------------------------------
-
-if mode == 'float':  # convert between fixed and floating strike
-    fac = -1.0
-    K = S_join[-1]  # stock price at maturity S(T) - see https://en.wikipedia.org/wiki/Asian_option
-else:
-    fac = 1.0
-
-
-# ----------------------------------------
-# ARITHMETIC
-# ----------------------------------------
-
-# Continuous sampling
-AC_c_join = DF * np.maximum(fac * A_c_join - fac * K, 0)
-AC_c = np.mean(AC_c_join[-1])
-AC_c_e = np.std(AC_c_join[-1]) / I  # error std/sqrt(N)
-
-# --------------------------------------------------
-# Variables into Dictionary
-# --------------------------------------------------
-
-t_index[-1] = 1.0  # ensures plots do not extend the x-axis to account for slight rounding error
-
-dic = {'S0': S0, 'K': K, 'T': T, 'r': r, 'sigma': sigma, 'M': M, 'I': I, 'k': k,  # params
-       't_index': t_index,
-       'S_join': S_join,
-       'A_c_join': A_c_join,
-       'A_d_join': A_d_join,
-       'G_c_join': G_c_join,
-       'V_join': V_join,
-       'V': V,
-       'V_e': V_e,
-       'AC_c_join': AC_c_join,
-       'AC_d_join': AC_d_join,
-       'GC_c_join': GC_c_join,
-       'AC_c': AC_c,
-       'AC_c_e': AC_c_e,
-       'AC_d': AC_d,
-       'AC_d_e': AC_d_e,
-       'GC_c': GC_c,
-       'GC_c_e': GC_c_e,
-       'mode': mode
-       }
-
-
-
-# ---------------------------------------------------------------------------------------
-# ---------------------------------------------------------------------------------------
-
-dic = asian_option_simulator(S0=100., K=100., T=1.0, r=0.05, sigma=0.2, M=100, I=100, k=10, mode='fixed')
-
+# # # Seaborn pointplot (inefficient)
+#
+# ax1 = sns.pointplot(np.arange(I), my_cap_plus, scale=0.5)
+# ax1.xaxis.set_visible(False)
+# ax1.set_xlabel('No. of MC simulations')
+# ax1.set_ylabel('Caplet price')
+# ax2 = sns.pointplot(np.arange(I), my_cap_join, scale=0.5, color="#bb3f3f")
+# ax2.xaxis.set_visible(False)
+#
+# # reduce number of x labels to plot
+# nbins = 100
+# x = np.arange(I)
+# my_x = x[0::nbins]
+# plt.locator_params(nbins=my_x.shape[0], axis='x')
+# ax1.xaxis.set_ticklabels(my_x)