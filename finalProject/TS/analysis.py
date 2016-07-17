6e7abab1be6caa3d04140fdaf033148df7a91f10
GBTSAG <Tanya.Sandoval@shell.com>
2016-07-17 15:48:42 +0100

    fixes to aic def in AR(p) model to match statsmodels - different to adfuller aic

--- finalProject/TS/analysis.py
+++ finalProject/TS/analysis.py
@@ -61,24 +61,7 @@ Y_t3 = pd.Series(Y_t3, name='Y_t3')
 from statsmodels.tsa.tsatools import (lagmat, add_trend)  # helper functions to add lags and trends
 
 
-def my_df_resid(exog=None, my_df=None):
-    """
-    Function to get degrees of freedom to calculate the scale of the OLS covariance matrix (see below)
-    :param exog: exogenous variable used to get its dimension
-    :param nobs: number of observations used to estimate parameters
-    :param my_df: if already know the df, can also plug instead of using default
-    :return:
-    """
-    if my_df is None:
-        # rank = np.ndim(exog)
-        nobs = exog.shape[0]
-        rank = exog.shape[1]
-        df = np.float(nobs - rank)
-    else:
-        df = my_df
-    return df
-
-def my_OLS(Y, X, df_resid=None):
+def my_OLS(Y, X):
     """
     Linear Regression implementation using Ordinary Least Squares (OLS) results
     :param Y: endogenous (dependent) variables
@@ -103,17 +86,13 @@ def my_OLS(Y, X, df_resid=None):
     # kron: kronecker product
     # diag: diagonal elements
     # See ref above or p.29 in  for more info
+    # Also calculate other useful values to store in result dictionary
 
-    nobs = len(resid_hat)  # number of observations
-
-    # The residual degree of freedom, defined as the number of observations minus the rank of the regressor matrix
-    if df_resid is None:  # if degrees of freedom not specified, then set to number of observations in resid_hat
-        df_resid = my_df_resid(exog=X)
-    else:
-        df_resid = my_df_resid(my_df=df_resid)
-
-    # Calculate other useful values to store in result dictionary
+    nobs = X.shape[0]
+    rank = X.shape[1]
+    df_resid = np.float(nobs - rank)
     ssr = np.dot(resid_hat, resid_hat.T)  # ee' term
+    sigma = ssr / nobs  # aka 'sigma2' or 'scale' in statsmodels
     ols_scale = ssr / df_resid  # ee' term must be scaled by df_resid to obtain unbiased estimate
     cov_params = np.kron(G, ols_scale)  # covariance matrix of parameters
     bvar = np.diag(cov_params)  # entries on the diagonal of the covariance matrix  are the variances
@@ -123,8 +102,7 @@ def my_OLS(Y, X, df_resid=None):
     llf = -nobs2 * np.log(2 * np.pi) - nobs2 * np.log(1 / (2 * nobs2) * \
                                                       np.dot(np.transpose(Y - np.dot(X, params)),
                                                              (Y - np.dot(X, params)))) - nobs2 # log-likelihood function of OLS model
-    df_model = X.shape[1]  # degrees of freedom of model
-    aic = -2 * llf + 2 * df_model  # Akaike information criterion
+    df_model = rank  # degrees of freedom of model
 
     dic = {
         'X': X,
@@ -134,13 +112,13 @@ def my_OLS(Y, X, df_resid=None):
         'nobs': nobs,
         'df_resid': df_resid,
         'ssr': ssr,
+        'sigma': sigma,
         'ols_scale': ols_scale,
         'cov_params': cov_params,
         'bse': bse,
         'tvalue': tvalue,
         'llf': llf,
         'df_model': df_model,
-        'aic': aic
            }
 
     return dic
@@ -163,13 +141,12 @@ def my_AR(endog, maxlag, trend=None):
     if trend is not None:
         X = add_trend(X, prepend=True, trend=trend)  # prepends puts trend column at the beginning
 
-    # Get degrees of freedom
-    nobs = len(Y)  # number of observations
-    k_ar = maxlag  # number of lags used, which affects number of observations
-    k_trend = 0  # the number of trend terms included 'nc'=0, 'c'=1
-    df_resid_AR = nobs - k_ar - k_trend  # degrees of freedom
+    result = my_OLS(Y, X)
+    # Akaike information criterion using statsmodel def for AR(p) (Lutkephol's definition)
+    # note this is diff to adfuller's def
+    result['aic'] = np.log(result['sigma']) + 2.0 * (1.0 + result['df_model']) / result['nobs']
 
-    return my_OLS(Y, X, df_resid=df_resid_AR)
+    return result
 
 
 def my_adfuller(y, maxlag=None):
@@ -192,16 +169,18 @@ def my_adfuller(y, maxlag=None):
     Y = ydshort  # endogenous var
     X = ydall[:, :maxlag + 1]  # exogenous var
 
-    result = my_OLS(Y, X, df_resid=None)  # do the usual regression using OLS to estimate parameters
+    result = my_OLS(Y, X)  # do the usual regression using OLS to estimate parameters
 
     # Add a few other info to the results dictionary
     result['adfstat'] = result['tvalue'][0] # define adfstat as tvalue of phi coefficient
     result['maxlag'] = maxlag
+    # Akaike information criterion using statsmodel def for adfuller - not this is different to the def in AR(p) model
+    result['aic'] = -2 * result['llf'] + 2 * result['df_model']
 
     return result
 
 
-def get_optimal_lag(y, maxlag):
+def get_optimal_lag(y, maxlag, model):
     """
     Returns the optimal lag for an adfuller model applied to the series y
     :param y: array, the series to apply the adfuller model
@@ -212,10 +191,13 @@ def get_optimal_lag(y, maxlag):
     results = {}
     startlag = 0  # loop from 0 up to maxlag
     for lag in range(startlag, startlag + maxlag + 1):
-        results[lag] = my_adfuller(y, maxlag=lag)
+        if model == 'adf':
+            results[lag] = my_adfuller(y, maxlag=lag)
+        elif model == 'ar':
+            results[lag] = my_AR(y, maxlag=lag)
+        print 'lag={0}, aic={1}'.format(lag, results[lag]['aic'])
         # Cross-check results vs statsmodels result - warning: small difference observed
         # sm_result = adfuller(x=y, maxlag=lag, regression='nc', autolag=None, regresults=True)[3].resols
-        print 'lag={0}, aic={1}'.format(lag, results[lag]['aic'])
         # print 'lag={0}, aic={1}, sm_aic={2}'.format(lag, results[lag]['aic'], sm_result.aic)
         # print 'lag={0}, nobs={1}, sm_nobs={2}'.format(lag, results[lag]['nobs'], sm_result.nobs)
         # print 'lag={0}, adfstat={1}, sm_adfstat={2}'.format(lag, results[lag]['adfstat'], sm_result.tvalues)
@@ -226,17 +208,21 @@ def get_optimal_lag(y, maxlag):
     return bestlag, icbest
 
 
-# =================   COMPARE MY GET_OPTIMAL_LAG VS STATSMODELS   =================
+# =================   COMPARE MY OPTIMAL LAG VS STATSMODELS FOR ADF  =================
 
-y = Y_t1
-my_lag = 20
+y = Y_t3
+my_lag = 35
 
-# My result, try a maximum of 20 lags
-bestlag , icbest = get_optimal_lag(y, maxlag=my_lag)
+# My result, try a maximum of lags equal to my_lag
+bestlag , icbest = get_optimal_lag(y, maxlag=my_lag, model='adf')
 
-# Statsmodel equivalent to optimise lag using aic criterion - will not be used as some inconsistencies seen (see comments below)
-sm_result = adfuller(x=y, maxlag=my_lag, regression='nc', autolag='AIC', regresults=True)
+# Statsmodel equivalent to optimise lag using aic criterion - will not use as some inconsistencies seen (see below)
+sm_bestlag = adfuller(x=y, maxlag=my_lag, regression='nc', autolag='AIC', regresults=True)[3].usedlag
+sm_icbest1 = adfuller(x=y, maxlag=sm_bestlag, regression='nc', autolag='AIC', regresults=True)[3].icbest
+sm_icbest2 = adfuller(x=y, maxlag=sm_bestlag, regression='nc', autolag=None, regresults=True)[3].resols.aic
+# sm_icbest1 and sm_icbest2 should be the same but are inconsitent
 
+sys.exit()
 # # Inconsistency observed in statsmodels for optimal lag selection using 'aic'
 # print 'sm_result[3].usedlag=', sm_result[3].usedlag  # should be == bestlag == my_result['maxlag'] but small diff observed
 # print 'sm_result[3].resols.df_model - 1=', sm_result[3].resols.df_model - 1  # equivalent to above
@@ -269,31 +255,44 @@ print "my_result['aic']={0}, sm_result[3].resols.aic={1}".format(my_result['aic'
 # my_result['llf'] == sm_result[3].resols.llf
 # my_result['X'].shape[1] == sm_result[3].resols.df_model  # rank of regressor matrix
 
+# =================   COMPARE MY OPTIMAL LAG VS STATSMODELS FOR AR(P)  =================
 
-# =================   COMPARE MY AR(p) VS STATSMODELS   =================
+y = Y_t3
+my_lag = 35
 
-maxlag = 3
-my_result = my_AR(endog=y, maxlag=maxlag)
+# My result, try a maximum of lags equal to my_lag
+bestlag , icbest = get_optimal_lag(y, maxlag=my_lag, model='ar')
 
-# Compare to statsmodels AR(p) model
-sm_result = AR(np.array(y)).fit(maxlag=maxlag, trend='nc')  # use only specified lags and remove constant
+# Statsmodel equivalent to optimise lag using aic criterion - will not use as some inconsistencies seen (see below)
+sm_bestlag = AR(np.array(y)).select_order(maxlag=my_lag, ic='aic', trend='nc', method='cmle')
+sm_icbest = AR(np.array(y)).fit(maxlag=sm_bestlag, trend='nc', method='cmle').aic
+# above line inconsistent since this isn't the lower aic found, e.f. maxlag=29 for Y_t3 gives aic=-2.45, which is lower
+# than sm_bestlag=7 with aic=-2.40
 
-# # Cross-checks
-# print "\
-# AR.fit.params={0} \n MY params={1} \n\
-# AR.fit.llf={2} \n MY llf={3} \n\
-# AR.fit.nobs={4} \n MY nobs={5} \n\
-# AR.fit.cov_params(scale=ols_scale)={6} \n MY cov_params={7} \n\
-# AR.fit.bse={8} \n MY bse={9} \n\
-# AR.fit.tvalues={10} \n MY tvalue={11} \n\
-# ".format(
-#     sm_result.params, my_result['params'],
-#     sm_result.llf, np.array(my_result['llf']),
-#     sm_result.nobs, my_result['nobs'],
-#     sm_result.cov_params(scale=my_result['ols_scale']), my_result['cov_params'],
-#     sm_result.bse, my_result['bse'],
-#     sm_result.tvalues, my_result['tvalue']
-# )
+
+# =================   COMPARE MY AR(P) VS STATSMODELS   =================
+
+# Instead I use my optimised lag (bestlag), which gives consistent results with statsmodels too
+
+my_result = my_AR(endog=y, maxlag=bestlag)
+sm_result = AR(np.array(y)).fit(maxlag=bestlag, trend='nc', method='cmle')
+
+# Cross-checks
+print "\
+AR.fit.params={0} \n MY params={1} \n\
+AR.fit.llf={2} \n MY llf={3} \n\
+AR.fit.nobs={4} \n MY nobs={5} \n\
+AR.fit.cov_params(scale=ols_scale)={6} \n MY cov_params={7} \n\
+AR.fit.bse={8} \n MY bse={9} \n\
+AR.fit.tvalues={10} \n MY tvalue={11} \n\
+".format(
+    sm_result.params, my_result['params'],
+    sm_result.llf, np.array(my_result['llf']),
+    sm_result.nobs, my_result['nobs'],
+    sm_result.cov_params(scale=my_result['ols_scale']), my_result['cov_params'],
+    sm_result.bse, my_result['bse'],
+    sm_result.tvalues, my_result['tvalue']
+)
 
 
 
